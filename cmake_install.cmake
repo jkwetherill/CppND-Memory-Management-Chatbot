9949129c242acfbdbea5b5a30577a7266da845b0
Julia Wetherill <julia.wetherill@kla-tencor.com>
2022-04-05 00:08:40 +0000

    Task 3

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/workspace/workspace3/CppND-Memory-Management-Chatbot
+# Install script for directory: /home/workspace/workspace2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/workspace/workspace3/CppND-Memory-Management-Chatbot/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/workspace/workspace2/CppND-Memory-Management-Chatbot/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
